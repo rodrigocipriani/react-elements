2fb16ad6884871f13f28dddf4147bf546dab5c2e
rodrigocipriani <chopsss@gmail.com>
2016-09-25 20:40:39 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,61 @@
+/* @flow */
+'use strict';
+
+/*!
+ * Pattern criado para que o projeto não crie dependência de quaisquer bibliotecas
+ *
+ * todo: importar elementos diferentes para ios e android diretamente aqui. Ex.: DatePicker
+ * todo :implementar estrutura de Temas (themes)
+ *
+ * @author   Rodrigo Cipriani da Rosa <chopsss@gmail.com> <http://www.rodrigocipriani.com.br>
+ * @license  MIT
+ */
+
+
+// exemplo:
+// não importa de onde está vindo o Text, o App funciona da mesma maneira
+import {
+    AppRegistry,
+    StyleSheet,
+    View,
+    DrawerLayoutAndroid,
+    Image,
+    Text,
+
+    TextInput,
+    ScrollView,
+    TouchableWithoutFeedback,
+    DatePickerAndroid,
+    TimePickerAndroid
+} from 'react-native';
+// import { Text } from 'native-base';
+
+// components
+import Button from './components/material-design/Button';
+import TextField from './components/Input/TextField';
+
+/**
+ * Vendors
+ * */
+
+
+module.exports = {
+    // ex.: NomeMaisAdequado: Component
+
+    // react-native components
+    AppRegistry: AppRegistry,
+    StyleSheet: StyleSheet,
+    View: View,
+    Drawer: DrawerLayoutAndroid,
+    Image: Image,
+    Text: Text,
+    TextInput: TextInput,
+    ScrollView: ScrollView,
+    TouchableWithoutFeedback: TouchableWithoutFeedback,
+    DatePicker: DatePickerAndroid,
+    TimePicker: TimePickerAndroid,
+
+    // material designs components
+    Button: Button,
+    TextField: TextField
+};
